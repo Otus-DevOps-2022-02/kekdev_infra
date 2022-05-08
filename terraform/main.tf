5fe3a9f6f07af0a116701561c06f067e288cd3b3
Constantine Lozhkin <6170034+kekdev@users.noreply.github.com>
2022-05-09 04:44:26 +0500

    split main.tf separate files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "~> 0.35"
     }
   }
@@ -13,59 +13,4 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
-}
-
-resource "yandex_compute_instance" "app" {
-  count = 1
-  name  = "reddit-app${count.index}"
-  zone  = var.resource_zone
-  resources {
-    cores  = 2
-    memory = 2
-  }
-  boot_disk {
-    initialize_params {
-      image_id = var.image_id
-    }
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.app-subnet.id
-    nat       = true
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type        = "ssh"
-    host        = self.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "yandex_vpc_network" "app-network" {
-  name = "reddit-app-network"
-}
-
-resource "yandex_vpc_subnet" "app-subnet" {
-  name           = "reddit-app-subnet"
-  zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.app-network.id}"
-  v4_cidr_blocks = ["192.168.10.0/24"]
 }
